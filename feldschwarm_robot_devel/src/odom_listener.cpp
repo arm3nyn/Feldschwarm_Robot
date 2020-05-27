28d48511434fb732d517a17f7110bb397363ce3a
Arman Oezavci <arman.oezavci@ivi.fraunhofer.de>
2020-05-27 11:24:29 +0200

    odom_listener node

--- feldschwarm_robot_devel/src/odom_listener.cpp
+++ feldschwarm_robot_devel/src/odom_listener.cpp
@@ -1,29 +1,56 @@
 #include <ros/ros.h>
 //#include <sensor_msgs/PointCloud2.h>
-#include <message_filters/subscriber.h> 
+#include <message_filters/subscriber.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Pose.h>
+
+//Publish odom_xy  laser_odom_xy delta_x  distance_gt_  distance_laserOdom deltaDistance
+
 class OdomListener{
 
 private:
     
-    ros::Subscriber odom_sub;
-    ros::Subscriber laser_odom_sub;
+    ros::NodeHandle n;
+    ros::Subscriber odomSub;
+    ros::Subscriber laserOdomSub;
+    ros::Publisher gtOdomPub;
+
+
 
 public:
-
-    void cb_laser(const sensor_msgs::PointCloud2::ConstPtr& msg)
-    {
-
-    }
-    void cb_odom(const)
-    {
-
-    }
+    nav_msgs::Odometry gt_odom;
+    nav_msgs::Odometry laser_odom;
     OdomListener()
     {
-        ros::NodeHandle n;
-        laser_odom_sub=n.subscribe("chatter",10,&OdomListener::cb_laser,this);
-        odom_sub=n.subscribe("chatter2",10,&OdomListener::cb_odom,this);
-    }   
+        gtOdomPub=n.advertise<nav_msg::Odometry>("odom_listener",1000);
+        laserOdomSub=n.subscribe("chatter",1000,&OdomListener::cb_laser,this);
+        odomSub=n.subscribe("chatter2",1000,&OdomListener::cb_odom,this);
+
+    }
+    ~OdomListener()
+    {
+
+    }
+    void cb_laser(const sensor_msgs::PointCloud2::ConstPtr& laser_msg)
+    { 
+        laser_odom.pose.pose.position.x=laser_msg->pose.pose.position.x;
+        laser_odom.pose.pose.position.y=laser_msg->pose.pose.position.y;
+
+
+    }
+    void cb_odom(const nav_msgs::Odometry::ConstPtr& odom_msg)
+    {
+        gt_odom.pose.pose.position.x=odom_msg->pose.pose.position.x;
+        gt_odom.pose.pose.position.y=odom_msg->pose.pose.position.y;
+        gtOdomPub.publish(gt_odom);
+
+
+    }
+    
+    
+        
+        
+      
 
 }; 
 
@@ -32,10 +59,7 @@ int main(int argc, char* argv[])
 ros::init(argc, argv,"OdomListener");
 OdomListener listenOdom;
 
-while(ros::ok)
-{
-
-}
+ros::spin();
 
 
 
